55dc318bbfc80d053f05bd129f9ed634a7119988
Chao Quenet <chaoyu1979@gmail.com>
2018-11-13 15:39:01 +0100

    ajoute déploiement de tag

--- scripts/deploy_review_app.sh
+++ scripts/deploy_review_app.sh
@@ -1,3 +1,3 @@
-echo "{ \"alias\": \"$TRAVIS_BRANCH${TRAVIS_REPO_SLUG/\//-}\" }" > now-review-app.json
+echo "{ \"alias\": \"production-${TRAVIS_REPO_SLUG/\//-}\" }" > now-production-app.json
 now --public --token $NOW_TOKEN
-now alias --token $NOW_TOKEN --local-config now-review-app.json
+now alias --token $NOW_TOKEN --local-config now-production-app.jsonapp.json
